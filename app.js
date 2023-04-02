5bafba99b097d0fd16408c2b4561c9e637c20560
Maha Sidi <94317341+mahasidi@users.noreply.github.com>
2023-04-02 15:24:35 -0400

    Sections 1-6

---
+++ app.js
@@ -0,0 +1,16 @@
+import HelloController
+    from "./controllers/hello-controller.js"
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController
+    from "./controllers/tuits/tuits-controller.js";
+
+
+import express from 'express';
+import cors from 'cors'
+const app = express()
+app.use(express.json());
+app.use(cors())
+TuitsController(app);
+HelloController(app)
+UserController(app)
+app.listen(4000)
