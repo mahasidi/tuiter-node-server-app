d4a88c6928282dd3a6456876236a2fa8c07429e0
Maha Sidi <94317341+mahasidi@users.noreply.github.com>
2023-04-09 16:00:16 -0400

    mongoDB addition

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
 import HelloController
     from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js";
